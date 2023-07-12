01002057c35e1e4e91df10c52a79670ca0c8fdce
Alain Iglesias <alain.iherrera@gmail.com>
2023-07-12 00:30:38 -0600

    added link 5

---
+++ src/components/link5.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react'
+import { TouchableOpacity, Text } from 'react-native'
+
+interface LinkProps {
+  text?: string
+}
+
+const Link3: FC<LinkProps> = ({ text = 'Go to' }) => {
+  return (
+    <TouchableOpacity>
+      <Text>{text}</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default Link3
